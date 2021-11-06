91df5820fd36fe7a657605699e6c1bf507be5c52
pangwenji <799953277@qq.com>
2021-11-06 21:02:08 +0800

    xx

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+   <div class="app">
+        <router-view />
+   </div>
+</template>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  background: #020308;
+  overflow: hidden;
+}
+</style>
